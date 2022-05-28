4fe68b0030edd8f59f89e8e28e0bdd11eac224d8
José Nascimento <vittorr.nascimento@gmail.com>
2022-05-27 21:40:03 -0300

    node.js

---
+++ file 4 rocket/NPM.md
@@ -0,0 +1,9 @@
+Glossary: dependencies, packages,modules__
+
+-[] Verificar se temos o npm instalado `npm-v` 
+-[] Criar nosso própria pacote
+-[] O que é o arquivo
+-[] Utilizar módulos de terceiros
+-[] O que é o diretório node_modules
+-[] O que é o arquivo package-lock.json
+-[] criar scripts para rodar com o npm
---
+++ file 4 rocket/index.js
@@ -0,0 +1,6 @@
+// npm init -y é mais rápido do que npm init
+//otras formas de instalar dependecias
+//exemplo: npm i cfontes, o i é um atalho para install
+//npm i cfontes -D : é uma dependecia somente de desenvolvimento isso significa que só vai ser usado o package em um ambiente de desenvolvimento
+// o npm install permite que se instale varios
+// npm update permite atualizar se houve alguma mudança
