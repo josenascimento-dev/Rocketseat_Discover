c8b3fa5e5b5aad1a21f18c946e121a37cf8a46fd
José Nascimento <vittorr.nascimento@gmail.com>
2022-06-01 21:44:54 -0300

    sql

---
+++ file 6 rocket/aula.js
@@ -0,0 +1,24 @@
+//passo 1: Modelando
+class Queue {
+    constructor(){
+        this.data = []
+    }
+    enqueue(item){
+        this.data.push(item)
+        console.log(`${item} chegou na fila`);
+    }
+    dequeue(){
+        const item = this.data.shift()
+        console.log(`${item} saiu da fila`);
+    }
+}
+
+//2:utilizando
+const fila =new Queue()
+
+fila.enqueue('Mariana')
+fila.enqueue('João')
+fila.enqueue('Arieal')
+fila.dequeue()
+fila.dequeue()
+fila.dequeue()
