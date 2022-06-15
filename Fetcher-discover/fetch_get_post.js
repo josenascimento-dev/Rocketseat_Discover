d307be9f9df7e763d9f2713107ec464aaa7b7d2d
José Nascimento <vittorr.nascimento@gmail.com>
2022-06-15 16:35:28 -0300

    API with Fetch

---
+++ Fetcher-discover/fetch_get_post.js
@@ -0,0 +1,42 @@
+const url="http://localhost:5500/api"
+
+function getUsers() {
+    fetch(url)
+       .then(response => response.json())
+       .then(data => renderApiResult.textContent = JSON.stringify(data))
+       .catch(error => console.error(error))
+}
+function getUser(){
+    fetch(`${url}/2`)
+    .then(response => response.json())
+    .then(data => {
+        userName.textContent = data.name
+        userCity.textContent = data.city
+        userAvatar.src = data.avatar
+    })
+    .catch(error => console.error(error))
+}
+function addUser(newUser){
+    fetch(url, {
+        method: "POST",
+        body: JSON.stringify(newUser),
+        headers:{
+         "content-type": "application/json; charset=UTF-8"
+        }
+    })
+       .then(response => response.json())
+       .then(data => alertApi.textContent = data)
+       .catch(error => console.error(error))
+
+
+}
+const newUser = {
+    name: "Olivia Zars",
+    avatar: "http://picsum.photos/200/300",
+    city: "Rio do Sul"
+}
+
+addUser(newUser)
+
+getUsers
+getUser()
